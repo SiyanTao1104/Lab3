8626297922f996c2262818b3db97e48fb52d7c3f
SiyanTao1104 <114071083@qq.com>
2022-09-20 15:46:09 +0800

    Upload the change files for QTpacman

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by TSY on 2022/9/17.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World nooo?";
+    return 0;
+}
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+// Created by TSY on 2022/9/17.
+//
+
+#ifndef LAB3_HELLO_H
+#define LAB3_HELLO_H
+
+
+class hello {
+
+};
+
+
+#endif //LAB3_HELLO_H
